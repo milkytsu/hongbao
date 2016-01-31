d7607ddeb047ae284abf2fa8d9aa12bb9469309e
Milky <eplug1986@gmail.com>
2016-02-01 00:30:22 +0800

    add hongbao class file

---
+++ hongbao.php
@@ -0,0 +1,36 @@
+<?php
+class Hongbao
+{
+    private $hongbao = [];
+    private $minMoney = 0.01; // 每个人可领取的最小金额（默认1分）
+    // 创建红包
+    public function create($num = 0, $totalMoney = 0) 
+    {
+        if (!is_numeric($num) || empty($num) || empty($totalMoney) || $totalMoney / $num < $this->minMoney) {
+            return false;
+        }
+        // 生成红包id
+        $id = uniqid();
+        // 剩余金额
+        $remainingMoney = $totalMoney;
+        // 简单算法，抽中的金额必须保证剩余金额能够使剩余每个抽中的金额至少是minMoney
+        for ($i = 1; $i < $num; ++$i) { 
+            $maxMoney = $remainingMoney - ($num - $i) * $this->minMoney;
+            $money = mt_rand($this->minMoney * 100, $maxMoney * 100) / 100;
+            $this->hongbao[$id][] = $money;
+            $remainingMoney -= $money;
+        }
+        shuffle($this->hongbao[$id]);
+
+        return $id;
+    }
+
+    // 领取红包
+    public function get($id)
+    {
+        if empty($this->hongbao[$id]) {
+            return false;
+        }
+        return array_unshift($this->hongbao[$id]);
+    }
+}
